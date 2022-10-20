35552f74a9c43cbd526dfa2998b2de288d0a619f
VadimL <badgunn@yandex.ru>
2022-10-20 18:28:24 +0400

    FinApp v1.0

---
+++ src/ExpensesManager.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList; // Импортируйте ArrayList
+
+public class ExpensesManager {
+    ArrayList<Double> expenses = new ArrayList<>();; // Замените массив списком
+
+    ExpensesManager() {
+       // ArrayList<Double> expenses = new ArrayList<>(); // Создайте список в конструкторе
+    }
+    // Номер дня больше не нужен
+    double saveExpense(double moneyBeforeSalary, double expense) {
+        moneyBeforeSalary = moneyBeforeSalary - expense;
+        expenses.add(expense); // Эту строку нужно убрать
+        System.out.println("Значение сохранено! Ваш текущий баланс в рублях: " + moneyBeforeSalary);
+        if (moneyBeforeSalary < 1000) {
+            System.out.println("На вашем счету осталось совсем немного. Стоит начать экономить!");
+        }
+        return moneyBeforeSalary;
+    }
+
+    void printAllExpenses() {
+        for (int i=0; i < expenses.size(); i++) {
+            System.out.println("Трата № " + (i + 1) + ". Потрачено " + expenses.get(i) + " рублей");
+        }
+    }
+
+    double findMaxExpense() {
+        double maxExpense = 0.0;
+        for (Double expense : expenses) { // Используйте сокращённую форму цикла
+            if (expense > maxExpense) {
+                maxExpense = expense;
+            }
+        }
+        return maxExpense;
+    }
+}
